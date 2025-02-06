337e09dd2bb9ed8f868dbd7456a96cbd2a4fca2d
russkyc <jrcc1023@gmail.com>
2025-02-07 03:31:04 +0800

    :tada: Start project

---
+++ presyotect-next/Program.cs
@@ -0,0 +1,60 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(options => { options.RouteTemplate = "/openapi/{documentName}.json"; });
+    app.MapScalarApiReference(endpointPrefix:"_/scalar",options =>
+    {
+        options.Theme = ScalarTheme.DeepSpace;
+        options.Authentication = new ScalarAuthenticationOptions()
+        {
+            ApiKey = new ApiKeyOptions()
+            {
+                Token = "tast"
+            }
+        };
+    });
+}
+
+app.UseStaticFiles();
+app.MapFallbackToFile("index.html");
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+var group = app.MapGroup("_/api");
+
+group.MapGet("/weather", () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
