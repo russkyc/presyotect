235c98192eff83adaede264c7ccc139b307d6f40
russkyc <jrcc1023@gmail.com>
2025-03-05 11:29:59 +0800

    :recycle: enable scalar docs in deployment

--- presyotect-next/Program.cs
+++ presyotect-next/Program.cs
@@ -21,17 +21,14 @@ app.UseCors(
 app.UseAuthentication();
 app.UseAuthorization();
 
-#if DEBUG
 app.UseSwagger(options => { options.RouteTemplate = "/openapi/{documentName}.json"; });
-    app.MapScalarApiReference(endpointPrefix: "_api_docs", options =>
-    {
-        options.Theme = ScalarTheme.Kepler;
-    });
-    app.MapGet("/", (context) =>
-    {
-        context.Response.Redirect("/_api_docs/");
-        return Task.CompletedTask;
-    });
+app.MapScalarApiReference(endpointPrefix: "_api_docs", options => { options.Theme = ScalarTheme.Kepler; });
+#if DEBUG
+app.MapGet("/", (context) =>
+{
+    context.Response.Redirect("/_api_docs/");
+    return Task.CompletedTask;
+});
 #else
 app.UseStaticFiles();
 app.MapFallbackToFile("index.html");
